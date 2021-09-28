a8db14ef7ab5ed2372732780140dc45bc7c0df18
nazneen-158 <nazneensulthana158@gmail.com>
2021-09-28 23:09:25 +0530

    Added: employee Attendance

--- employeewage.sh
+++ employeewage.sh
@@ -1,1 +1,12 @@
 echo "Hello Welcome !!"
+
+
+ispresent=0
+isabsent=1
+randomnumber=$((RANDOM%2))
+if      [ $randomnumber -eq $ispresent ]
+then
+        echo "Employee is present"
+else
+        echo "Employee is absent"
+fi
